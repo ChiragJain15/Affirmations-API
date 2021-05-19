c5682775b0541aab8079d66da9d0072c04e72cc5
chirag <chiragjain150302@gmail.com>
2021-05-19 11:44:20 +0530

    added a very small burst animation using mo.js on click

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const textbox = document.getElementById("main_heading");
 const affirmation =
   affirmations[Math.floor(Math.random() * affirmations.length)];
 textbox.innerHTML = affirmation;
+
+const burst = new mojs.Burst({
+    scale: 2,
+    count: 10 
+});
+
+
+document.addEventListener("click", function (e) {
+  burst.replay();
+});
