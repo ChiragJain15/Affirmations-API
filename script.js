044bd908f125a66617e20c29bb68b326f954a491
Chirag Jain <chiragjain150302@gmail.com>
2021-05-20 20:51:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const textbox = document.getElementById("main_heading");
 
+ let affirmation =
+  affirmations[Math.floor(Math.random() * affirmations.length)];
+textbox.innerHTML = affirmation;
+
 const burst = new mojs.Burst({
     scale: 2,
     count: 10 
@@ -8,7 +12,7 @@ const burst = new mojs.Burst({
 
 document.addEventListener("click", function (e) {
   burst.replay();
-  const affirmation =
+  affirmation =
   affirmations[Math.floor(Math.random() * affirmations.length)];
 textbox.innerHTML = affirmation;
 
